4cfded2cf020d24c14b682d0f351fea7a79bc411
Emil Munnksø <emil_lukas@hotmail.com>
2017-06-11 08:51:56 +0200

    Vinter16-17 first

---
+++ Vinter16-17/Vinter16-17/Images.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinter16_17
+{
+    public class Images
+    {
+        private string _currentSelected;
+        public string CurrentSelected
+        {
+            get { return _currentSelected; }
+            set { _currentSelected = value; }
+        }
+    }
+}
---
+++ Vinter16-17/Vinter16-17/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+
+namespace Vinter16_17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string ChoosenPath = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fdb = new FolderBrowserDialog();
+            fdb.ShowNewFolderButton = false;
+            var result = fdb.ShowDialog();
+            while (true)
+            {
+                if ((fdb.SelectedPath) != "")
+                {
+                    ChoosenPath = fdb.SelectedPath;
+                    System.Windows.MessageBox.Show(ChoosenPath);
+
+                    addItems();
+
+                    break;
+                }
+
+            }
+
+        }
+
+        public void addItems()
+        {
+            this.List.Items.Clear();
+            tbxFileName.Text = ChoosenPath;
+            //var ext = new List<string> { "jpg", "gif", "png" };
+            var list = System.IO.Directory.GetFiles(ChoosenPath, "*.*", System.IO.SearchOption.TopDirectoryOnly)
+               .Where(file => new string[] { ".jpg", ".gif", ".png" }
+                .Contains(System.IO.Path.GetExtension(file)))
+                .ToList();
+
+            foreach (var file in list)
+                this.List.Items.Add(System.IO.Path.GetFileName(file));
+        }
+
+        private void List_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var image = new BitmapImage(new Uri(this.tbxFileName.Text + "\\" + this.List.SelectedItem.ToString()));
+
+            this.CurrentImage.Source = image;
+            this.CurrentImage.Stretch = Stretch.Uniform;
+        }
+
+        private void NewName_Click(object sender, RoutedEventArgs e)
+        {
+            this.CurrentImage.Source = null;
+            copyAndMove(this.tbxFileName.Text + "\\", this.NewFileName.Text);
+            addItems();
+        }
+
+
+
+        public void copyAndMove(string oldName, string newName)
+        {
+            var index = 0;
+            DirectoryInfo d = new DirectoryInfo(oldName);
+            FileInfo[] infos = d.GetFiles();
+            foreach (FileInfo f in infos)
+            {
+                File.Move(f.FullName, this.tbxFileName.Text + "\\"+ newName + $" - {++index}" + ".jpg");
+            }
+        }
+    }
+}
